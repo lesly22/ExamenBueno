c86924d63cccbc37cd1aab73bf45e15b7dd9f0e3
lesly22 <lesm_guerram@unicah.edu>
2020-10-15 16:32:10 -0600

    agregar handlebars

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Importar express
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 
 // Crear un servidor express
 const app = express();
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+
 
 // Inicializar el servidor en un puerto en específico
 app.listen(5000, () => {
