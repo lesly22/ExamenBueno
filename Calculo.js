d834ae6df22f1f73d151d413df8946d76951e6e0
lesly22 <lesm_guerram@unicah.edu>
2020-10-15 21:47:49 -0600

    ji

---
+++ Calculo.js
@@ -0,0 +1,24 @@
+exports.Calculo =(capital,saldoFinal,periodo,intereses) => {
+
+    let capital = 0;
+    let saldoFinal = 0;
+    let periodo = 0;
+    let intereses = intereses / 0;
+    
+    periodo = Math.round(perido* 12);
+
+
+    intereses = capital * intereses;
+    intereses = intereses.toFixed(2);
+
+    periodo++;
+
+    capital=capital;
+    capital=capital.toFixed(2);
+    
+
+    
+
+    
+
+}
